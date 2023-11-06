56950d6e2e793f51b27f530ee003b1250b512b3d
Marcos Leao <marcosplaignier@hotmail.com>
2023-11-05 21:33:16 -0300

    Iniciando tipo Receita

---
+++ src/app/shared/model/tipo.receita.ts
@@ -0,0 +1,12 @@
+export class TipoReceita{
+
+  id: number;
+  nome: string;
+  situacao: boolean;
+  dataCriacao: Date;
+  dataInativacao:Date;
+  temTaxa: any;
+  percentualTaxa: number;
+  versao: number;
+
+}
