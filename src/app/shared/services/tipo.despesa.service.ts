01f53e0da5dd76afed448d98604a230fac555aed
Marcos Leao <marcosplaignier@hotmail.com>
2023-11-05 21:32:28 -0300

    Iniciando tipo despesa service e crud

--- src/app/shared/services/tipo.despesa.service.ts
+++ src/app/shared/services/tipo.despesa.service.ts
@@ -1,5 +1,4 @@
 import {AbstractCrudService} from "./abstract.crud.service";
-import {TipoReceita} from "../model/tipo.receita";
 import {HttpClient} from "@angular/common/http";
 import {Injectable} from "@angular/core";
 import {TipoDespesa} from "../model/tipo.despesa";
